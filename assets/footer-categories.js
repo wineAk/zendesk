49a990db34d9ce20b6f279d0fb3458a33927e834
wineAk <akasaka@interpark.co.jp>
2025-03-08 13:04:00 +0900

    [update] フッターのカテゴリーデザインを変更

--- assets/footer-categories.js
+++ assets/footer-categories.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const html = categories.map(category => {
       const { html_url, name } = category
       return `
-      <a href="${html_url}" class="border border-slate-200 rounded-lg hover:bg-gray-50 no-underline">
+      <a href="${html_url}" class="border border-slate-200 rounded-lg hover:bg-gray-50 no-underline w-32">
         <div class="px-4 py-2 text-gray-500 text-center">
           ${name}
         </div>
