420fa2831f41358a46aa91905fb776d9c71f083a
wineAk <akasaka@interpark.co.jp>
2025-03-14 15:32:34 +0900

    [add] アコーディオンにアニメーションを追加

--- assets/article_page-sidemenu.js
+++ assets/article_page-sidemenu.js
@@ -43,33 +43,35 @@ document.addEventListener('DOMContentLoaded', function () {
         const { articles } = section
         const articlesHtml = articles.map(article => `<div class="truncate pl-2"><a href="${article.html_url}" title="${article.name}">${article.name}</a></div>`).join('')
         return `
-        <div class="flex flex-col gap-2 bg-gray-50 rounded-md p-2" data-accordion="collapse">
-          <div class="flex items-center gap-1">
-            <button class="hover:bg-gray-200 rotate-270">
-              <svg aria-hidden="true" class="w-6 h-6 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <div class="flex flex-col gap-2 bg-gray-50 rounded-md p-2 pb-0" data-accordion="collapse">
+          <div class="flex items-center gap-1 ">
+            <button class="rounded-full hover:bg-gray-200 transition-transform duration-200">
+              <svg aria-hidden="true" class="w-6 h-6 rotate-270 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd">
                 </path>
               </svg>
             </button>
-            <a href="${section.html_url}" title="${section.name}">${section.name}</a>
+            <a href="${section.html_url}" title="${section.name}" class="truncate">${section.name}</a>
           </div>
-          <div class="hidden flex-col gap-2">
-            ${articlesHtml}
+          <div class="h-0 overflow-hidden transition-all duration-200" style="interpolate-size: allow-keywords;">
+            <div class="flex flex-col gap-2 p-2">
+              ${articlesHtml}
+            </div>
           </div>
         </div>`
       }).join('')
       const html = `
-        <div class="flex flex-col gap-2 bg-gray-50 rounded-md p-2" data-accordion="collapse">
+        <div class="flex flex-col gap-2 bg-gray-50 rounded-md p-2 pb-0" data-accordion="collapse">
           <div class="flex items-center gap-1">
-            <button class="hover:bg-gray-200 rotate-270">
-              <svg aria-hidden="true" class="w-6 h-6 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <button class="rounded-full hover:bg-gray-200 transition-transform duration-200">
+              <svg aria-hidden="true" class="w-6 h-6 rotate-270 pointer-events-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd">
                 </path>
               </svg>
             </button>
             <a href="${category.html_url}">${category.name}</a>
           </div>
-          <div class="hidden flex-col gap-2">
+          <div class="h-0 overflow-hidden transition-all duration-200" style="interpolate-size: allow-keywords;">
             ${sectionsHtml}
           </div>
         </div>`
@@ -79,17 +81,14 @@ document.addEventListener('DOMContentLoaded', function () {
     // ボタンクリック
     document.querySelectorAll('[data-accordion="collapse"]').forEach(collapseElm => {
       collapseElm.querySelector('button').addEventListener('click', event => {
+        // ボタンの回転
         const buttonElm = event.target
+        buttonElm.classList.toggle('rotate-90')
+        // セクションの表示
         const categoryElm = buttonElm.closest('[data-accordion="collapse"]')
         const sectionsElm = categoryElm.lastElementChild
-        const isSectionsHidden = sectionsElm.classList.contains('hidden')
-        if (isSectionsHidden) {
-          sectionsElm.classList.replace('hidden', 'flex')
-          buttonElm.classList.remove('rotate-270')
-        } else {
-          sectionsElm.classList.replace('flex', 'hidden')
-          buttonElm.classList.add('rotate-270')
-        }
+        sectionsElm.classList.toggle('h-0')
+        sectionsElm.classList.toggle('max-h-screen')
       })
 
     })
