de739fd73834fc1ac336f00677faf31abf94385e
wineAk <akasaka@interpark.co.jp>
2025-04-03 10:39:54 +0900

    [update] モーダルをsweetalert2に変更

--- assets/article_page-image_modal.js
+++ assets/article_page-image_modal.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.article-body img').forEach(elm => {
-    // 画像に `x-on:click` を動的に追加
-    elm.setAttribute('x-on:click', `modalOpen = true; imgSrc = '${elm.src}'`)
-    // Alpine.js に要素を認識させる
-    Alpine.initTree(elm)
+    elm.addEventListener('click', event => {
+      const { target } = event
+      const { src } = target
+      Swal.fire({
+        imageUrl: src,
+        showConfirmButton: false,
+      });
+    })
   })
 })
