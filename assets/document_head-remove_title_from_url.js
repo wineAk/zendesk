c4e8d4055a8cbe34e1a8d9494676e331d6c3e114
wineAk <akasaka@interpark.co.jp>
2025-08-29 16:39:50 +0900

    [fix] ハッシュが消える不具合を修正

--- assets/document_head-remove_title_from_url.js
+++ assets/document_head-remove_title_from_url.js
@@ -1,14 +1,14 @@
 /**
  * URLからタイトルを削除
- * @param {string} url /hc/ja/articles/35749118295321-%E3%82%B9%E3%82%B3%E3%82%A2-%E8%A8%AD%E5%AE%9A
- * @returns /hc/ja/articles/35749118295321
+ * @param {string} url /hc/ja/articles/35749118295321-%E3%82%B9%E3%82%B3%E3%82%A2-%E8%A8%AD%E5%AE%9A#h_01JN02ADF0PX22M8YSG9VCP0NV
+ * @returns /hc/ja/articles/35749118295321#h_01JN02ADF0PX22M8YSG9VCP0NV
  */
 function removeTitle(url) {
-  return url.replace(/(categories|sections|articles)(\/\d+)(\-.*)/, '$1$2') 
+  return url.replace(/(categories|sections|articles)(\/\d+)(\-[^#]*)(#.*)?/, '$1$2$4') 
 }
 
-// 現在のページ
-const pathname = window.location.pathname
+// 現在のページ（pathname + hash）
+const pathname = window.location.pathname + window.location.hash
 const replacePath = removeTitle(pathname)
 if (pathname !== replacePath) history.replaceState(null, '', replacePath)
 
